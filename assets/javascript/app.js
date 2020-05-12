da19c8882e5199a95b4d37381f1b1f2c073b01b8
Ryan Strickler <ryan_s@knights.ucf.edu>
2020-05-12 14:47:47 -0400

    fixed logic and added comments

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,357 +1,155 @@
+// config firebase
 var config = {
-    apiKey: "AIzaSyCSCdkQH0zqSN5DksGq-fcesTHrzVYcjCM",
-    authDomain: "project-1-6255c.firebaseapp.com",
-    databaseURL: "https://project-1-6255c.firebaseio.com",
-    projectId: "project-1-6255c",
-    storageBucket: "project-1-6255c.appspot.com",
-    messagingSenderId: "899461180848"
+    apiKey: "AIzaSyD__acBsqBMONtyV5wp4SAjI3i0R9Q8Gy8",
+    authDomain: "rock-paper-scissors-6655c.firebaseapp.com",
+    databaseURL: "https://rock-paper-scissors-6655c.firebaseio.com",
+    projectId: "rock-paper-scissors-6655c",
+    storageBucket: "rock-paper-scissors-6655c.appspot.com",
+    messagingSenderId: "1007602444840",
+    appId: "1:1007602444840:web:8b400907feef80103ae31d",
+    measurementId: "G-PC91NYWTHT"
 };
+
 firebase.initializeApp(config);
 var database = firebase.database();
-
 var player = {
     choice: "",
     wins: 0,
     losses: 0,
     ties: 0
 }
-
+// hold player child's key
 var playerID;
-var choice;
-var gameInProgress = false;
+// player reference used to remove player when they dc
+var playerRef;
 
-database.ref("/Players").once("value", function (snapshot) { //initializing firebase
-    if (snapshot.val() === null) {
-        database.ref("/Players/Player1").set(player);
-        playerID = 1;
-    }
-    else {
-        database.ref("/Players/Player2").set(player);
-        playerID = 2;
-    }
-}, function (errorObject) { //error handler function
-    console.log("The read failed: " + errorObject.code);
-});
+$(document).ready(() => {
 
-$("#submit").on("click", function () {
-    var string = "Player " + playerID + ": ";
-    string += $("#chat-entry").val() + '\n';
-    database.ref("/Chat").push(string);
-    $("#chat-entry").val('');
-});
-
-database.ref("/Chat").on("child_added", function (snapshot) {
-    console.log(snapshot.val());
-    $("#chatDisplay").append(snapshot.val());
-});
-
-$("#rock").on("click", function () {
-    $("#rpsSelect").hide();
-    $("#playerChoicePic").html("<img src=assets/images/rock.png>");
-    choice = "rock";
-    if (playerID === 1) {
-        database.ref("/Players/Player1/choice").set(choice);
-    }
-    else {
-        database.ref("/Players/Player2/choice").set(choice);
-    }
-    checkChoices();
-});
-
-$("#paper").on("click", function () {
-    $("#rpsSelect").hide();
-    $("#playerChoicePic").html("<img src=assets/images/paper.jpeg>");
-    choice = "paper";
-    if (playerID === 1) {
-        database.ref("/Players/Player1/choice").set(choice);
-    }
-    else {
-        database.ref("/Players/Player2/choice").set(choice);
-    }
-    checkChoices();
-});
-
-$("#scissors").on("click", function () {
-    $("#rpsSelect").hide();
-    $("#playerChoicePic").html("<img src=assets/images/scissors.jpeg>");
-    choice = "scissors";
-    if (playerID === 1) {
-        database.ref("/Players/Player1/choice").set(choice);
-    }
-    else {
-        database.ref("/Players/Player2/choice").set(choice);
-    }
-    checkChoices();
-});
-
-function checkChoices() {
-    database.ref("/Players").on("value", function (snapshot) { //initializing firebase
-        //console.log(snapshot.val());
-        var players = snapshot.val();
-        if (players.Player2 !== undefined) {
-            if (players.Player1.choice != "" && players.Player2.choice != "") {
-                if (!gameInProgress) {
-                    play(players);
-                }
-            }
+    database.ref("/Players").once("value", function (snapshot) {
+        // if there are less than 2 players, push new player
+        if (snapshot.val() === null || snapshot.numChildren() < 2) {
+            playerRef = database.ref("/Players").push(player);
+            playerID = playerRef.key;
+            console.log(playerID)
+            $("#welcome-text").html("<h3>Welcome Player " + playerID + "</h3>");
+            // remove player if they dc
+            playerRef.onDisconnect().remove().then(() => {
+                console.log(playerRef);
+            });
         }
+        // if there are already 2 players, prevent user from joining game
+        else {
+            playerID = "Guest"
+            $("#rpsSelect").hide();
+            $("#welcome-text").html("<h3>Sorry, there are already 2 players. Please come back later.</h3>");
+        }
+        $("#welcome-modal").modal("show");
     }, function (errorObject) { //error handler function
         console.log("The read failed: " + errorObject.code);
     });
 
-}
-
-function play(players) {
-    gameInProgress = true;
-    var oppChoice;
-    var wins;
-    var losses;
-    var ties;
-    if (playerID === 1) {
-        oppChoice = players.Player2.choice;
-    }
-    else {
-        oppChoice = players.Player1.choice;
-    }
-
-    if (choice == "rock" && oppChoice == "rock") {
-        $("h1").html("You tied!");
-        $("#playerDisplay").html("You chose " + choice);
-        $("#oppDisplay").html("Your opponent chose " + oppChoice);
-        $("#oppChoicePic").html("<img src=assets/images/rock.png>");
-
-        if (playerID === 1) {
-            ties = parseInt(players.Player1.ties);
-            ties++;
-            database.ref("/Players/Player1/ties").set(ties);
-        }
-        else {
-            ties = parseInt(players.Player2.ties);
-            ties++;
-            database.ref("/Players/Player2/ties").set(ties);
-        }
-
-        setTimeout(function () {
-            reset()
-        }, 5000);
-
-    }
-
-    if (choice == "rock" && oppChoice == "paper") {
-        $("h1").html("You lost!");
-        $("#playerDisplay").html("You chose " + choice);
-        $("#oppDisplay").html("Your opponent chose " + oppChoice);
-        $("#oppChoicePic").html("<img src=assets/images/paper.jpeg>");
-
-        if (playerID === 1) {
-            losses = parseInt(players.Player1.losses);
-            losses++;
-            database.ref("/Players/Player1/losses").set(losses);
-        }
-        else {
-            losses = parseInt(players.Player2.losses);
-            losses++;
-            database.ref("/Players/Player2/losses").set(losses);
-        }
-
-        setTimeout(function () {
-            reset()
-        }, 5000);
-    }
-
-    if (choice == "rock" && oppChoice == "scissors") {
-        $("h1").html("You won!");
-        $("#playerDisplay").html("You chose " + choice);
-        $("#oppDisplay").html("Your opponent chose " + oppChoice);
-        $("#oppChoicePic").html("<img src=assets/images/scissors.jpeg>");
-
-        if (playerID === 1) {
-            wins = parseInt(players.Player1.wins);
-            wins++;
-            database.ref("/Players/Player1/wins").set(wins);
-        }
-        else {
-            wins = parseInt(players.Player2.wins);
-            wins++;
-            database.ref("/Players/Player2/wins").set(wins);
-        }
-
-        setTimeout(function () {
-            reset()
-        }, 5000);
-
-    }
-
-    if (choice == "paper" && oppChoice == "rock") {
-        $("h1").html("You won!");
-        $("#playerDisplay").html("You chose " + choice);
-        $("#oppDisplay").html("Your opponent chose " + oppChoice);
-        $("#oppChoicePic").html("<img src=assets/images/rock.png>");
-
-        if (playerID === 1) {
-            wins = parseInt(players.Player1.wins);
-            wins++;
-            database.ref("/Players/Player1/wins").set(wins);
-        }
-        else {
-            wins = parseInt(players.Player2.wins);
-            wins++;
-            database.ref("/Players/Player2/wins").set(wins);
-        }
-
-        setTimeout(function () {
-            reset()
-        }, 5000);
-
-    }
-
-    if (choice == "paper" && oppChoice == "paper") {
-        $("h1").html("You tied!");
-        $("#playerDisplay").html("You chose " + choice);
-        $("#oppDisplay").html("Your opponent chose " + oppChoice);
-        $("#oppChoicePic").html("<img src=assets/images/paper.jpeg>");
-
-        if (playerID === 1) {
-            ties = parseInt(players.Player1.ties);
-            ties++;
-            database.ref("/Players/Player1/ties").set(ties);
-        }
-        else {
-            ties = parseInt(players.Player2.ties);
-            ties++;
-            database.ref("/Players/Player2/ties").set(ties);
-        }
-
-        setTimeout(function () {
-            reset()
-        }, 5000);
-
-    }
-
-    if (choice == "paper" && oppChoice == "scissors") {
-        $("h1").html("You lost!");
-        $("#playerDisplay").html("You chose " + choice);
-        $("#oppDisplay").html("Your opponent chose " + oppChoice);
-        $("#oppChoicePic").html("<img src=assets/images/scissors.jpeg>");
-
-        if (playerID === 1) {
-            losses = parseInt(players.Player1.losses);
-            losses++;
-            database.ref("/Players/Player1/losses").set(losses);
-        }
-        else {
-            losses = parseInt(players.Player2.losses);
-            losses++;
-            database.ref("/Players/Player2/losses").set(losses);
-        }
-
-        setTimeout(function () {
-            reset()
-        }, 5000);
-
-    }
-
-    if (choice == "scissors" && oppChoice == "rock") {
-        $("h1").html("You lost!");
-        $("#playerDisplay").html("You chose " + choice);
-        $("#oppDisplay").html("Your opponent chose " + oppChoice);
-        $("#oppChoicePic").html("<img src=assets/images/rock.png>");
-
-        if (playerID === 1) {
-            losses = parseInt(players.Player1.losses);
-            losses++;
-            database.ref("/Players/Player1/losses").set(losses);
-        }
-        else {
-            losses = parseInt(players.Player2.losses);
-            losses++;
-            database.ref("/Players/Player2/losses").set(losses);
-        }
-
-        setTimeout(function () {
-            reset()
-        }, 5000);
-
-    }
-
-    if (choice == "scissors" && oppChoice == "paper") {
-        $("h1").html("You won!");
-        $("#playerDisplay").html("You chose " + choice);
-        $("#oppDisplay").html("Your opponent chose " + oppChoice);
-        $("#oppChoicePic").html("<img src=assets/images/paper.jpeg>");
-
-        if (playerID === 1) {
-            wins = parseInt(players.Player1.wins);
-            wins++;
-            database.ref("/Players/Player1/wins").set(wins);
-        }
-        else {
-            wins = parseInt(players.Player2.wins);
-            wins++;
-            database.ref("/Players/Player2/wins").set(wins);
-        }
-
-        setTimeout(function () {
-            reset()
-        }, 5000);
-    }
-
-    if (choice == "scissors" && oppChoice == "scissors") {
-        $("h1").html("You tied!");
-        $("#playerDisplay").html("You chose " + choice);
-        $("#oppDisplay").html("Your opponent chose " + oppChoice);
-        $("#oppChoicePic").html("<img src=assets/images/scissors.jpeg>");
-
-        if (playerID === 1) {
-            ties = parseInt(players.Player1.ties);
-            ties++;
-            database.ref("/Players/Player1/ties").set(ties);
-        }
-        else {
-            ties = parseInt(players.Player2.ties);
-            ties++;
-            database.ref("/Players/Player2/ties").set(ties);
-        }
-
-        setTimeout(function () {
-            reset()
-        }, 5000);
-    }
-}
-
-function reset() {
-    database.ref("/Players/Player1/choice").set("");
-    database.ref("/Players/Player2/choice").set("");
-    $("#rpsSelect").show();
-    $("#oppChoicePic").html("");
-    $("#playerChoicePic").html("");
-    $("#playerDisplay").html("You");
-    $("#oppDisplay").html("Opponent");
-    $("h1").html("Rock Paper Scissors!");
-    gameInProgress = false;
-    append();
-}
-
-function append() {
-
-    database.ref("/Players").once("value", function (snapshot) {
-        players=snapshot.val();
-        if (playerID === 1) {
-            $("#wins").html("Wins: " + players.Player1.wins);
-            $("#losses").html("Losses: " + players.Player1.losses);
-            $("#ties").html("Ties: " + players.Player1.ties);
-            $("#oppWins").html("Wins: " + players.Player2.wins);
-            $("#oppLosses").html("Losses: " + players.Player2.losses);
-            $("#oppTies").html("Ties: " + players.Player2.ties);
-        }
-        else {
-            $("#wins").html("Wins: " + players.Player2.wins);
-            $("#losses").html("Losses: " + players.Player2.losses);
-            $("#ties").html("Ties: " + players.Player2.ties);
-            $("#oppWins").html("Wins: " + players.Player1.wins);
-            $("#oppLosses").html("Losses: " + players.Player1.losses);
-            $("#oppTies").html("Ties: " + players.Player1.ties);
-        }
+    // handle player selection
+    $(".playerSelect").on("click", function () {
+        $("#rpsSelect").hide();
+        player.choice = $(this).attr("data");
+        $("#playerChoicePic").html("<img src=assets/images/" + player.choice + ".jpeg>");
+        database.ref(playerRef).set(player);
     });
-}
 
+    // check to see if both players are present and have chosen an option
+    database.ref("/Players").on("value", function (snapshot) {
+        // loop through all players
+        snapshot.forEach((playerChild) => {
+            // opponent is the player with a different key than the player
+            if (playerChild.key !== playerID) {
+                var opp = playerChild.val();
+                if (opp.choice !== "" && player.choice !== "") {
+                    play(opp);
+                }
+            }
+        });
+    });
+
+    // game logic, show match results to players
+    function play(opp) {
+        // tie conditions
+        if (player.choice === opp.choice) {
+            $("h1").html("You tied!");
+            $("#playerDisplay").html("You chose " + player.choice);
+            $("#oppDisplay").html("Your opponent chose " + opp.choice);
+            $("#oppChoicePic").html("<img src=assets/images/" + opp.choice + ".jpeg>");
+            player.ties++;
+            opp.ties++;
+            setTimeout(function () {
+                reset(opp)
+            }, 5000);
+        }
+        // loss conditions 
+        else if (player.choice == "rock" && opp.choice == "paper" || player.choice == "scissors" && opp.choice == "rock" || player.choice == "paper" && opp.choice == "scissors") {
+            $("h1").html("You lost!");
+            $("#playerDisplay").html("You chose " + player.choice);
+            $("#oppDisplay").html("Your opponent chose " + opp.choice);
+            $("#oppChoicePic").html("<img src=assets/images/" + opp.choice + ".jpeg>");
+            player.losses++;
+            opp.wins++;
+            setTimeout(function () {
+                reset(opp)
+            }, 5000);
+        }
+        // win conditions 
+        else {
+            $("h1").html("You won!");
+            $("#playerDisplay").html("You chose " + player.choice);
+            $("#oppDisplay").html("Your opponent chose " + opp.choice);
+            $("#oppChoicePic").html("<img src=assets/images/" + opp.choice + ".jpeg>");
+            player.wins++;
+            opp.losses++;
+            setTimeout(function () {
+                reset(opp)
+            }, 5000);
+        }
+    }
+
+    // reset player choices and display, update player data in database
+    function reset(opp) {
+        player.choice = "";
+        playerRef.set(player);
+        $("#rpsSelect").show();
+        $("#oppChoicePic").html("");
+        $("#playerChoicePic").html("");
+        $("#playerDisplay").html("Select your choice");
+        $("#oppDisplay").html("Waiting for Opponent");
+        $("h1").html("Rock Paper Scissors!");
+        append(opp);
+    }
+
+    // update scoreboard after a match
+    function append(opp) {
+        $("#wins").html("Wins: " + player.wins);
+        $("#losses").html("Losses: " + player.losses);
+        $("#ties").html("Ties: " + player.ties);
+        $("#oppWins").html("Wins: " + opp.wins);
+        $("#oppLosses").html("Losses: " + opp.losses);
+        $("#oppTies").html("Ties: " + opp.ties);
+    }
+
+    // push chat entry to database
+    $("#submit").on("click", function () {
+        var string;
+        if (playerID !== "Guest") {
+            string = "<b>Player " + playerID + ":</b> ";
+        }
+        else {
+            string = "<b>Guest:</b> "
+        }
+        string += $("#chat-entry").val() + "<br>";
+        database.ref("/Chat").push(string);
+        $("#chat-entry").val('');
+    });
+
+    // append new chat entry to display when added to database
+    database.ref("/Chat").on("child_added", function (snapshot) {
+        console.log(snapshot.val());
+        $("#chatDisplay").append(snapshot.val());
+    });
+});
